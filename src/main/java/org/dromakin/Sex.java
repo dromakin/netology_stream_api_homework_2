e8c2f4086240cb866b52c15d61ee16865954479a
dromakin <d.romakin@npo-echelon.ru>
2023-01-09 22:31:31 +0300

    add solved tasks

---
+++ src/main/java/org/dromakin/Sex.java
@@ -0,0 +1,18 @@
+/*
+ * File:     Sex
+ * Package:  org.dromakin
+ * Project:  netology_stream_api_homework_2
+ *
+ * Created by dromakin as 09.01.2023
+ *
+ * author - dromakin
+ * maintainer - dromakin
+ * version - 2023.01.09
+ */
+
+package org.dromakin;
+
+public enum Sex {
+    MAN,
+    WOMAN
+}
